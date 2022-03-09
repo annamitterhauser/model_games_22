804e0beaab4f11cb72fd17ecadc4fc18a20b6ee6
annamitterhauser <95572069+annamitterhauser@users.noreply.github.com>
2022-03-09 10:44:03 +0100

    Anna V01

---
+++ Assets/Game/Scripts/mouseInteraction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 50f) && hit.rigidbody != null)
+        {
+            hit.rigidbody.AddForce(new Vector3(0,2,0), ForceMode.Impulse);
+        }
+    }
+}
